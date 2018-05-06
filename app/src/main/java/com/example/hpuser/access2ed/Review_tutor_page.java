ab2f0cd21009c77a8702273ce2de0bd9cfdf8fca
YasminA1998 <yasminfathi90@gmail.com>
2018-05-05 22:35:43 -0400

    Tutor parts

---
+++ app/src/main/java/com/example/hpuser/access2ed/Review_tutor_page.java
@@ -0,0 +1,13 @@
+package com.example.hpuser.access2ed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Review_tutor_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_tutor_page);
+    }
+}
